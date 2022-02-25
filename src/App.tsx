ce1273637da501d72fa2f35fc0004d10b46c206b
yanachuwan9sm <gurem919@icloud.com>
2022-02-25 12:36:02 +0900

    update: yupでのフォームバリデーション

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Container, Stack, TextField } from "@mui/material";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 // フォームの型
 interface SampleFormInput {
@@ -9,8 +11,30 @@ interface SampleFormInput {
   password: string;
 }
 
+const schema = yup.object({
+  email: yup
+    .string()
+    .required("必須内容です")
+    .email("正しいメールアドレスを入力して下さい"),
+  name: yup.string().required("必須内容です"),
+  password: yup
+    .string()
+    .required("必須内容です")
+    .min(6, "文字数が少なすぎます。6文字以上で入力して下さい。")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&].*$/,
+      "英字と数字と記号が最低1文字必要です！"
+    ),
+});
+
 const App: React.VFC = () => {
-  const { register, handleSubmit } = useForm<SampleFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SampleFormInput>({
+    resolver: yupResolver(schema),
+  });
 
   // フォーム送信時の処理
   const onSubmit: SubmitHandler<SampleFormInput> = (data) => {
@@ -22,10 +46,35 @@ const App: React.VFC = () => {
     <>
       <Container maxWidth="sm" sx={{ pt: 5 }}>
         <Stack spacing={3}>
-          <TextField required label="メールアドレス" type="email" />
-          <TextField required label="お名前" />
-          <TextField required label="パスワード" type="password" />
-          <Button color="primary" variant="contained" size="large">
+          <TextField
+            required
+            label="メールアドレス"
+            type="email"
+            {...register("email")}
+            error={"email" in errors}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            required
+            label="お名前"
+            {...register("name")}
+            error={"name" in errors}
+            helperText={errors.name?.message}
+          />
+          <TextField
+            required
+            label="パスワード"
+            type="password"
+            {...register("password")}
+            error={"password" in errors}
+            helperText={errors.password?.message}
+          />
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            onClick={handleSubmit(onSubmit)}
+          >
             作成
           </Button>
         </Stack>
